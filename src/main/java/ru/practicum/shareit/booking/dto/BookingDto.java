513c9ce1e02f7c4dd77cd67788b4725dea89df58
Ivan Berezin <uivan2009@rambler.ru>
2026-02-10 17:24:23 +0300

    add first iter sprint 15: add-booking and add tests: fixed git validation

--- src/main/java/ru/practicum/shareit/booking/dto/BookingDto.java
+++ src/main/java/ru/practicum/shareit/booking/dto/BookingDto.java
@@ -20,16 +20,16 @@ public class BookingDto {
     @JsonInclude
     private Long id;
 
-    @NotNull
+    @NotNull(message = "не должно равняться null")
     private Long itemId;
 
-    @NotNull
-    @FutureOrPresent
+    @NotNull(message = "не должно равняться null")
+    @FutureOrPresent(message = "должно содержать сегодняшнее число или дату, которая еще не наступила")
     @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
     private LocalDateTime start;
 
-    @NotNull
-    @Future
+    @NotNull(message = "не должно равняться null")
+    @Future(message = "должно содержать дату, которая еще не наступила")
     @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
     private LocalDateTime end;
 }
